4536f460674f83fb5830f3247d29234ca517dc38
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2017-12-08 12:00:00 +0100

    Add remove() compare() implementations and tests

--- src/two_pset.erl
+++ src/two_pset.erl
@@ -6,20 +6,30 @@ new() ->
   {[], []}.
 
 lookup(E, {A, R}) ->
-  lists:member(E, A) and not lists:member(E, R).
+  is_member(E, A) and not is_member(E, R).
 
 add(E, S) ->
   case lookup(E, S) of
-    true -> S;
-    _    -> {A, R} = S,
-            {[E|A], R}
+    true  -> S;
+    _     -> {A, R} = S,
+             {[E|A], R}
   end.
 
-remove(_E, _S) ->
-  erlang:error(not_implemented).
+remove(E, S) ->
+  case lookup(E, S) of
+    true  -> {A, R} = S,
+             {A, [E|R]};
+    _     -> S
+  end.
 
-compare(_S1, _S2) ->
-  erlang:error(not_implemented).
+compare({A1, R1}, {A2, R2}) ->
+  lists:all(fun(X) -> is_member(X, A2) end, A1) and
+    lists:all(fun(X) -> is_member(X, R2) end, R1) and
+    lists:all(fun(X) -> is_member(X, A1) end, A2) and
+    lists:all(fun(X) -> is_member(X, R1) end, R2).
 
 merge(_S1, _S2) ->
   erlang:error(not_implemented).
+
+is_member(E, L) ->
+  lists:member(E, L).
--- test/two_pset_test.erl
+++ test/two_pset_test.erl
@@ -6,11 +6,11 @@ new_test() ->
   ?assertEqual(empty(), two_pset:new()).
 
 lookup_an_added_element_test() ->
-  S = from_lists([a], [b]),
+  S = from([a], [b]),
   ?assert(two_pset:lookup(a, S)).
 
 lookup_a_removed_element_test() ->
-  S = from_lists([a], [a, b]),
+  S = from([a], [a, b]),
   ?assertNot(two_pset:lookup(a, S)).
 
 add_new_element_test() ->
@@ -21,9 +21,34 @@ add_new_element_test() ->
   ?assert(same_values([a,b], SB )).
 
 add_same_element_twice_test() ->
-  S = from_lists([a], []),
+  S = from([a], []),
   ?assert(same_values([a], two_pset:add(a, S))).
 
+remove_element_test_() ->
+  [
+    remove_element_test(from([a], [a]), a, from([a], [])),
+    remove_element_test(from([a], []),  b, from([a], [])),
+    remove_element_test(from([a], [a]), a, from([a], [a]))
+  ].
+
+remove_element_test(R, E, S) ->
+  ?_assertEqual(R, two_pset:remove(E, S)).
+
+compare_two_sets_with_same_values_test() ->
+  A = from([a, b], [a]),
+  B = from([a, b], [a]),
+  ?assert(two_pset:compare(A, B)).
+
+compare_different_sets_test_() ->
+  [
+    compare_different_test(from([a], [a]), from([a, b], [a, b])),
+    compare_different_test(from([a,b], [a, b]), from([a], [a])),
+    compare_different_test(from([a], [a]), from([], [])),
+    compare_different_test(from([], []), from([a], [a]))
+  ].
+
+compare_different_test(A, B) ->
+  ?_assertNot(two_pset:compare(A, B)).
 
 %%
 %% Utils
@@ -31,7 +56,7 @@ add_same_element_twice_test() ->
 empty() ->
   {[], []}.
 
-from_lists(A, R) ->
+from(A, R) ->
   {A, R}.
 
 same_values(L, {A, R}) ->
