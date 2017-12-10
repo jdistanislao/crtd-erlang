adc9d8e97f490995d3f5536fc1effad064b429f4
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2017-12-10 20:10:32 +0100

    implements add, remove, lookup functions

--- src/orset.erl
+++ src/orset.erl
@@ -5,19 +5,36 @@
 -export([new/0, add/2, remove/2, lookup/2, compare/2, merge/2]).
 
 new() ->
-  erlang:error(not_implemented).
+  maps:new().
 
-add(_E, _S) ->
-  erlang:error(not_implemented).
+add(E, S) ->
+  {A, R} = maps:get(E, S, {sets:new(), sets:new()}),
+  maps:put(E, {sets:add_element(make_ref(), A), R}, S).
 
-remove(_E, _S) ->
-  erlang:error(not_implemented).
+remove(E, S) ->
+  case maps:is_key(E, S) of
+    true  -> {A, R} = maps:get(E, S),
+             maps:put(E, {A, sets:union(A, R)}, S);
+    _     -> S
+  end.
 
-lookup(_E, _S) ->
-  erlang:error(not_implemented).
+lookup(E, S) ->
+  case maps:is_key(E, S) of
+    true  -> is_element(maps:get(E, S));
+    _     -> false
+  end.
 
 compare(_S1, _S2) ->
   erlang:error(not_implemented).
 
 merge(_S1, _S2) ->
   erlang:error(not_implemented).
+
+%%
+%% Utils
+%%
+is_element({A, R}) ->
+  case sets:size(sets:subtract(A, R)) of
+    0 -> false;
+    _ -> {A, R}
+  end.
--- test/orset_test.erl
+++ test/orset_test.erl
@@ -4,4 +4,57 @@
 -include_lib("../src/crtd_records.hrl").
 
 
+new_test() ->
+  ?assertEqual(maps:new(), orset:new()).
 
+add_new_element_test() ->
+  SAdd = orset:add(a, orset:new()),
+  {A, _} = maps:get(a, SAdd),
+  ?assertEqual(1, sets:size(A)).
+
+add_new_element_twice_test() ->
+  S = orset:add(a, orset:new()),
+  SAdd = orset:add(a, S),
+  {A, _} = maps:get(a, SAdd),
+  ?assertEqual(2, sets:size(A)).
+
+add_different_elements_test() ->
+  S = orset:add(a, orset:new()),
+  SAdd = orset:add(b, S),
+  {AA, _} = maps:get(a, SAdd),
+  {BB, _} = maps:get(b, SAdd),
+  ?assertEqual(1, sets:size(AA)),
+  ?assertEqual(1, sets:size(BB)).
+
+remove_element_test() ->
+  S = orset:add(a, orset:new()),
+  SR = orset:remove(a, S),
+  {A, R} = maps:get(a, SR),
+  ?assertEqual(A, R).
+
+remove_element_twice_test() ->
+  S = orset:add(a, orset:new()),
+  SR1 = orset:remove(a, S),
+  SR = orset:remove(a, SR1),
+  {A, R} = maps:get(a, SR),
+  ?assertEqual(A, R).
+
+remove_a_non_existent_element_test() ->
+  S = orset:add(a, orset:new()),
+  SR = orset:remove(b, S),
+  {A, R} = maps:get(a, SR),
+  ?assertEqual(1, sets:size(A)),
+  ?assertEqual(0, sets:size(R)).
+
+lookup_an_added_element_test() ->
+  S = orset:add(a, orset:new()),
+  ?assertNotEqual(false, orset:lookup(a, S)).
+
+lookup_a_non_existent_element_test() ->
+  S = orset:add(a, orset:new()),
+  ?assertEqual(false, orset:lookup(b, S)).
+
+lookup_a_removed_element_test() ->
+  S = orset:add(a, orset:new()),
+  SR = orset:remove(a, S),
+  ?assertEqual(false, orset:lookup(a, SR)).
